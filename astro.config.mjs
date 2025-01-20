10c0226b9567d10a9f51196534eca13a7d285bea
Diego Yon <diegoyon@outlook.com>
2025-01-20 14:32:14 -0600

    add redirects

--- astro.config.mjs
+++ astro.config.mjs
@@ -23,7 +23,22 @@ export default defineConfig({
     tailwind(),
     partytown(),
   ],
-  redirects: {},
+  redirects: {
+    "/advanced_training": "/first-time-pilots#programs",
+    "/pilot-programs": "/first-time-pilots#programs",
+    "/our-courses": "/first-time-pilots#programs",
+    "/instrument-rating": "/programs/instrument-rating",
+    "/multi-engine-rating": "/programs/multi-engine-rating",
+    "/helicopter-pilot-training": "/programs/helicopter-pilot-training",
+    "/private-pilot-training": "/programs/private-pilot",
+    "/commercial-certificate": "/programs/commercial-pilot",
+    "/our-team": "/about/our-team",
+    "/contact-us": "/contact",
+    "/visit-ideal-aviation": "/contact",
+    "/about-ideal-aviation": "/about/our-story",
+    "/certified-flight-instructor": "/programs/cfi",
+    "/airplane-pilot-training": "/programs/private-pilot",
+  },
   vite: {
     resolve: {
       alias: {
