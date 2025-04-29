8a0e5927f67d9d9a4eb1c163c98b75070e17603b
Diego Yon <diegoyon@outlook.com>
2025-04-29 06:20:50 -0500

    add routes to team

---
+++ src/data/team.js
@@ -0,0 +1,79 @@
+const team = [
+  {
+    name: "Jim M.",
+    certifications: "Chief Flight Instructor, Fixed Wing CFI, CFII",
+    image: "/src/assets/images/jim_300.webp",
+  },
+  {
+    name: "Jake E.",
+    certifications: "Fixed Wing CFI, CFII",
+    image: "/src/assets/images/jake_300.webp",
+  },
+  {
+    name: "Don D.",
+    certifications: "Fixed Wing CFI",
+    image: "/src/assets/images/don-d-300-2.webp",
+  },
+  {
+    name: "Dan B.",
+    certifications: "Fixed Wing CFI",
+    image: "/src/assets/images/dan-b-300-2.webp",
+  },
+  {
+    name: "Kevin H.",
+    certifications: "Fixed Wing CFI",
+    image: "/src/assets/images/cfi-kevin-h.webp",
+  },
+  {
+    name: "Josh F.",
+    certifications: "Rotary Wing CFI, CFII",
+    image: "/src/assets/images/josh_f_300.webp",
+  },
+  {
+    name: "Joe G.",
+    certifications: "Fixed Wing CFI, CFII",
+    image: "/src/assets/images/joe-g-300.webp",
+  },
+  {
+    name: "Eric B.",
+    certifications: "Rotary Wing CFI",
+    image: "/src/assets/images/eric-b-300.webp",
+  },
+  {
+    name: "Andrew D.",
+    certifications: "Fixed Wing CFI",
+    image: "/src/assets/images/ideal_cfi_andrew_d.webp",
+  },
+  {
+    name: "Andy H.",
+    certifications: "Fixed Wing CFI, CFII, MEI",
+    image: "/src/assets/images/silhouette.webp",
+  },
+  {
+    name: "Dillon J.",
+    certifications: "Fixed Wing CFI, MEI",
+    image: "/src/assets/images/silhouette.webp",
+  },
+  {
+    name: "Mike W.",
+    certifications: "Fixed Wing CFI, CFII",
+    image: "/src/assets/images/cfii-mike-w.webp",
+  },
+  {
+    name: "Dean P.",
+    certifications: "Fixed Wing CFI, CFII, MEI",
+    image: "/src/assets/images/silhouette.webp",
+  },
+  {
+    name: "Alma K.",
+    certifications: "Fixed Wing CFI",
+    image: "/src/assets/images/cfii-alma-k.webp",
+  },
+  {
+    name: "Daniel S.",
+    certifications: "Fixed Wing CFI, MEI",
+    image: "/src/assets/images/silhouette.webp",
+  },
+];
+
+export default team;
